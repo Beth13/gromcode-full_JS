b0572f855d7821d77ac36a857f0e53345555c69e
JeePik <andrewlegeza@gmail.com>
2022-03-26 19:08:10 +0200

    added files lesson21 task4

---
+++ lesson21/task4/index.js
@@ -0,0 +1,24 @@
+export function getTitle() {
+  const elemTitle = document.querySelector(".title");
+  return elemTitle.textContent;
+}
+
+export function getDescription() {
+  const elemDescription = document.querySelector(".about");
+  return elemDescription.innerText;
+}
+
+export function getPlans() {
+  const elemPlans = document.querySelector(".plans");
+  return elemPlans.innerHTML;
+}
+
+export function getGoal() {
+  const elemGoal = document.querySelector(".goal");
+  return elemGoal.outerHTML;
+}
+
+console.log(getTitle());
+console.log(getDescription());
+console.log(getPlans());
+console.log(getGoal());
