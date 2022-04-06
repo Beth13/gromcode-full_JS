748878fca6eff53e81413f8d7d215025492876a9
JeePik <andrewlegeza@gmail.com>
2022-04-06 19:50:28 +0300

    added files lesson24 task2-3

---
+++ lesson24/task3/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const getDiff = (startDate, endDate) => {
+  const timeDiff = Math.abs(endDate.getTime() - startDate.getTime());
+
+  const daysDiff = Math.floor(timeDiff / 1000 / 3600 / 24);
+  const hoursDiff = Math.floor((timeDiff / 1000 / 3600) % 24);
+  const minutesDiff = Math.floor((timeDiff / 1000 / 60) % 60);
+  const secondsDiff = Math.floor((timeDiff / 1000) % 60);
+
+  return `${daysDiff}d ${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`;
+};
+
+console.log(getDiff(new Date(2022, 0, 1), new Date()));
