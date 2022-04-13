405f25ed7d1994b4779423d7fadf64006c8e07e2
JeePik <andrewlegeza@gmail.com>
2022-04-13 13:15:56 +0300

    added files lesson27 task1

---
+++ lesson27/task1/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+localStorage.clear();
+localStorage.setItem("hobbies", JSON.stringify({ name: "Tom" }));
+localStorage.setItem("name", "Tom");
+localStorage.setItem("age", JSON.stringify(17));
+
+export const getLocalStorageData = () => {
+  return Object.entries(localStorage).reduce((acc, [key, value]) => {
+    let newValue;
+    try {
+      newValue = JSON.parse(value);
+    } catch (e) {
+      newValue = value;
+    }
+
+    return {
+      ...acc,
+      [key]: newValue,
+    };
+  }, {});
+};
+
+console.log(getLocalStorageData());
