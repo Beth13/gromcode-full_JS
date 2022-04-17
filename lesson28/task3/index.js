6d79b8d14b4c31ff2c7353b81461b78aeb0a468b
JeePik <andrewlegeza@gmail.com>
2022-04-17 12:41:24 +0300

    added files lesson28 task3

---
+++ lesson28/task3/index.js
@@ -0,0 +1,11 @@
+"use strict";
+
+export const mult = (a) => (b) => a * b;
+
+console.log(mult(5)(4));
+
+export const twice = mult(2);
+export const triple = mult(3);
+
+console.log(twice(7));
+console.log(triple(9));
