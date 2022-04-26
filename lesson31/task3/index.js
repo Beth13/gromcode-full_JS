11f29d558f8ca0ec90137d5e3f7185ef6024bdc2
JeePik <andrewlegeza@gmail.com>
2022-04-26 20:05:08 +0300

    added lesson31 task2-3

---
+++ lesson31/task3/index.js
@@ -0,0 +1,38 @@
+const successPromise = new Promise((resolve) => {
+  resolve(32);
+});
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+
+successPromise
+  .then((number) => {
+    const halfNumber = number / 2;
+    return halfNumber;
+  })
+  .then((number) => {
+    /* fix this handler */
+    // console.log(`Num is: ${number}`);
+    const squaredNumber = number * number;
+    return squaredNumber;
+  })
+  .then((result) => {
+    console.log(result); // 256
+  });
+
+/*
+ * исправь цепочку промисов, чтобы в последнем обработчике вывелось нужное число
+ */
+successPromise
+  .then((number) => {
+    const result = number * 10;
+    return result;
+  })
+  .then((result) => {
+    console.log(result); // 320
+  });
+
+console.log(
+  "!!! Обрати внимание, что этот текст вывелся самым первым. Ведь это синхронный код, а промисы - асинхронны !!!"
+);
