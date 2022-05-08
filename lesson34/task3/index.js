3feff35139936e93b00c09fb579bf9f926113940
JeePik <andrewlegeza@gmail.com>
2022-05-08 12:23:21 +0300

    added files lesson34 task3

---
+++ lesson34/task3/index.js
@@ -0,0 +1,51 @@
+const baseUrl = "https://6272c6bfa6522e24ac3e537b.mockapi.io/v1/users";
+
+const formElem = document.querySelector(".login-form");
+const buttonElem = document.querySelector(".submit-button");
+const emailInput = document.querySelector('input[name = "email"]');
+const userNameInput = document.querySelector('.form-input[name="name"]');
+const userPassInput = document.querySelector('.form-input[name="password"]');
+
+// ----------------------------------------------------
+const isFormValidCheck = () => {
+  const isFormDone = formElem.reportValidity();
+
+  if (isFormDone) {
+    buttonElem.removeAttribute("disabled");
+  }
+};
+
+// ----------------------------------------------------
+formElem.addEventListener("input", isFormValidCheck);
+// ----------------------------------------------------
+function createUser(userData) {
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify(userData),
+  });
+}
+
+// ----------------------------------------------------
+const onCreateUserHandler = (event) => {
+  event.preventDefault();
+
+  const newUser = {
+    email: emailInput.value,
+    userName: userNameInput.value,
+    password: userPassInput.value,
+  };
+
+  createUser(newUser)
+    .then((response) => response.json())
+    .then((user) => alert(JSON.stringify(user)))
+    .then(() => {
+      emailInput.value = "";
+      userNameInput.value = "";
+      userPassInput.value = "";
+    });
+};
+
+buttonElem.addEventListener("click", onCreateUserHandler);
